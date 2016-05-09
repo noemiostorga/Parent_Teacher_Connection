8d3ba3e3dc0f42c5215980a808ff0f29da3f6390
User <user@Users-MacBook-Pro.local>
2016-05-09 12:20:28 -0700

    captures email and password

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+	var email = document.querySelector(".email").value;
+	var password = document.querySelector(".password").value;
+
+	return console.log(email + " " +password);
+
+}
+
